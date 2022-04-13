4dd419afc619ba68e7a401167cf6e60057252811
Mohammed Abdul Hafeez Khan <82383549+hafeezkhan909@users.noreply.github.com>
2022-04-13 15:22:19 +0400

    Update detection.py

--- detection.py
+++ detection.py
@@ -64,3 +64,17 @@ test_set = test_datagen.flow_from_directory(
 
 
 classifier.fit(x = training_set, validation_data = test_set, epochs = 25)
+
+
+import numpy as np
+from keras.preprocessing import image
+test_image = image.load_img('dataset\ce1dd162-881b-4097-89b4-eccee085fbfa___Matt.S_CG 0732.jpg', target_size = (256,256))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis = 0)
+result = classifier.predict(test_image)
+training_set.class_indices
+if result[0][0] == 1:
+  prediction = 'non_healthy_tomato'
+else:
+  prediction = 'healthy_tomato'
+print(prediction)
